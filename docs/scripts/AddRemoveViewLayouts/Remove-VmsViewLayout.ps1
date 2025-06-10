5a188db06adc0b983f595b9d26dc0ded6978ecca
joshooaj <7669972+joshooaj@users.noreply.github.com>
2025-06-10 14:21:23 -0700

    chore: Fix PSScriptAnalyzer warnings for new example
    The recently-added custom view layouts example triggers a PSUseShouldProcessForStateChangingFunctions warning on Remove-VmsViewLayout. The best solution is to add an if-should-process block but we can improve on the example later on.

--- docs/scripts/AddRemoveViewLayouts/Remove-VmsViewLayout.ps1
+++ docs/scripts/AddRemoveViewLayouts/Remove-VmsViewLayout.ps1
@@ -25,24 +25,24 @@
         The software provided by Milestone Systems, Inc. (hereinafter referred to as "the Software") is provided on
         an "as is" basis, without any warranties or representations, express or implied, including but not limited to
         the implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
-        
+
         Warranty Disclaimer:
         The Software is provided without any warranty of any kind, whether expressed or implied. Milestone Systems, Inc.
         expressly disclaims all warranties, conditions, and representations, including but not limited to warranties of
         title, non-infringement, merchantability, or fitness for a particular purpose. The entire risk arising out of the
         use or performance of the Software remains with the user.
-    
+
         Support Disclaimer:
         Milestone Systems, Inc. does not provide any support or maintenance services for the Software. The user acknowledges
         and agrees that Milestone Systems, Inc. shall have no obligation to provide any updates, bug fixes, or technical
         support for the Software, whether through telephone, email, or any other means.
-    
+
         User Responsibility:
         The user acknowledges and agrees that they are solely responsible for the selection, installation, use, and results
         obtained from the Software. Milestone Systems, Inc. shall not be held liable for any errors, defects, or damages arising
         from the use or inability to use the Software, including but not limited to direct, indirect, incidental, consequential,
         or special damages.
-    
+
         Indemnification:
         The user agrees to indemnify, defend, and hold harmless Milestone Systems, Inc. and its directors, officers, employees,
         and agents from any and all claims, liabilities, damages, losses, costs, and expenses (including reasonable attorneys' fees)
@@ -50,8 +50,9 @@
 
         By using the Software, the user acknowledges that they have read and understood this clause and agree to be bound by its terms.
     #>
-    
+
     [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification='Example function.')]
     param (
         [Parameter(Mandatory, ParameterSetName = 'Remove')]
         [string]
@@ -71,7 +72,7 @@
         break
     }
 
-    $layoutGroups = $ms.LayoutGroupFolder.LayoutGroups    
+    $layoutGroups = $ms.LayoutGroupFolder.LayoutGroups
     $customViews = New-Object System.Collections.Generic.List[PSCustomObject]
     if ($ListCustomLayouts) {
         foreach ($lg in $layoutGroups) {
@@ -85,7 +86,7 @@
                 $customViews.Add($row)
             }
         }
-        
+
         if (-not [string]::IsNullOrEmpty($customViews.'View Layout Folder')) {
             return $customViews
             break
@@ -101,6 +102,6 @@
         Write-Warning 'The selected view does not exist in the selected view layout group.'
         break
     }
-    
+
     $null = $layoutGroup.LayoutFolder.RemoveLayout($layout.Path)
 }
